ae7da6e4accc495592dd3336478892b84a0c588f
James Sharpe <JMTS@MacBook-Pro.fios-router.home>
2016-12-07 13:02:14 -0500

    start angular2 project and create homepage

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/templates/landing.component.html',
+  styleUrls: ['assets/styles/base.css'],
+})
+export class AppComponent  { name = 'Angular'; }
