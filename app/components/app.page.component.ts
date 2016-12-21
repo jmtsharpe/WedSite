21df6652b166827ee5e3e4f441dea1250b0e311d
James <jmtsharpe@gmail.com>
2016-12-20 20:43:04 -0500

    get working child component

---
+++ app/components/app.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'blank',
+	templateUrl: 'app/templates/app.blank.component.html',
+	styleUrls: ['assets/styles/movingpage.css'],
+})
+export class BlankComponent {
+	
+}
